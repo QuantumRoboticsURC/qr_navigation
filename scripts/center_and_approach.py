3fabb4c937197e85c379b16597bfcfdcbca7ddf4
QuantumRoboticsURC <quantumrobotics.itesm@gmail.com>
2023-01-27 17:22:48 -0600

    .

--- scripts/center_and_approach.py
+++ scripts/center_and_approach.py
@@ -5,7 +5,7 @@ from geometry_msgs.msg import Point, Twist
 
 
 class CenterAndApproach():
-    def _init_(self):
+    def __init__(self):
         # ___ ros atributes initialization ___
         rospy.init_node("matrix_signal_reciever")
         rospy.Subscriber("/closest_aruco_distance", Point, self.closest_aruco_callback, queue_size=1)
@@ -13,12 +13,14 @@ class CenterAndApproach():
 
         
         # ___ gains initialization ___
-        self.kp_angle_error = 0.00125   # comes from .4[rad/s] = +-320 [max error pixels] * kpae 
-        self.kp_distance_error = 0.2    # comes from  1[rad/s] = +-5 [max error meters] * kpde
+        self.kp_angle_error = -0.00125   # comes from .4[rad/s] = +-320 [max error pixels] * kpae 
+        self.kp_distance_error = 0.1    # comes from  1[rad/s] = +-5 [max error meters] * kpde
 
         # ___ error initialization ___
         self.angle_error = 0
         self.distance_error = 0
+        print("1")
+
         print(self.angle_error)
         # ___ vel command ___
         self.command_velocity = Twist()
@@ -30,15 +32,15 @@ class CenterAndApproach():
     def main(self):
         while not rospy.is_shutdown():
             print(self.angle_error)
-            if abs(self.angle_error) > 15:  
-                self.command_velocity.linear.x = 0.0
-                self.command_velocity.angular.z = self.angle_error * self.kp_angle_error
+            #if abs(self.angle_error) > 15:  
+            #self.command_velocity.linear.x = 0.0
+            self.command_velocity.angular.z = self.angle_error * self.kp_angle_error
             
-            else:
-                self.command_velocity.linear.x = self.distance_error * self.kp_distance_error
-                self.command_velocity.angular.z = 0.0
+            #else:
+            self.command_velocity.linear.x = self.distance_error * self.kp_distance_error
+            #self.command_velocity.angular.z = 0.0
 
-            self.command_velocity_publisher.publish(command)
+            self.command_velocity_publisher.publish(self.command_velocity)
 
 if __name__ == "__main__":
     center_and_approach = CenterAndApproach()
