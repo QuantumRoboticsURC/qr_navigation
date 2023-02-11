quaternion to euler corrected

scripts/tests/quaternionToEulerTest.py
@@ -1,4 +1,5 @@
 import math
+from scipy.spatial.transform import Rotation
 
 def euler_from_quaternion(w, x, y, z):
     """
@@ -6,7 +7,7 @@ def euler_from_quaternion(w, x, y, z):
     roll is rotation around x in radians (counterclockwise)
     pitch is rotation around y in radians (counterclockwise)
     yaw is rotation around z in radians (counterclockwise)
-    """
+    """    
     t0 = +2.0 * (w * x + y * z)
     t1 = +1.0 - 2.0 * (x * x + y * y)
     roll_x = math.atan2(t0, t1)
@@ -21,3 +22,5 @@ def euler_from_quaternion(w, x, y, z):
     yaw_z = math.atan2(t3, t4)
     
     return roll_x, pitch_y, yaw_z # in radians
+    
+    