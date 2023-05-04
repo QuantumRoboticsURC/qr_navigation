9e084c2df7f3c3b468c808828a2ba475d8d6662c
iker <A01749675@tec.mx>
2023-05-04 16:13:53 -0600

    Update GUI

--- scripts/set_target_client.py
+++ scripts/set_target_client.py
@@ -2,20 +2,22 @@ import rospy
 from coords.srv import set_target, set_targetRequest
 from tkinter import *
 from inspect import *
+from tkinter import ttk
 
-def client(latitud,longitud):
-    set_target_function = rospy.ServiceProxy('get_target', set_target)
+def client(latitud,longitud,message):
+    print(message)
     result=set_target_function(latitud,longitud)
     print(result.latitud_r)
     print(result.longitud_r)
 
 if __name__ == '__main__':
     try:
+        set_target_function = rospy.ServiceProxy('get_target', set_target)
         window = Tk()
         window.geometry("500x500")
         window.configure(bg="black")
-        Title = Label(window,width=20,height=3,text="Coordinate Publisher",fg="blue",bg="white",font=("TimesNEwRoman",15))
-        Title.place(x=150,y=0)
+        Title = Label(window,width=20,height=3,text="Coordinate Publisher",fg="blue",bg="white",font=("TimesNewRoman",15))
+        Title.place(x=140,y=0)
         LatitudLabel = Label(window,width=20,height=3,text="Latitud",background="black",fg="white")
         LatitudLabel.place(x=0,y=100)
         latitud = Text(width=20,height=4,bg="white")
@@ -24,7 +26,9 @@ if __name__ == '__main__':
         LongitudLabel.place(x=0,y=200)
         longitud = Text(width=20,height=4,bg="white")
         longitud.place(x=150,y=200)
-        publish = Button(window,width=10,height=4,text="publish",fg="blue",bg="black",command=lambda m="F":client(float(latitud.get("1.0","end-1c")),float(longitud.get("1.0","end-1c"))))
+        combo = ttk.Combobox(state="readonly",values=["gps_and_post","gps_and_gate","gps_only"])
+        combo.place(x=150,y=300)
+        publish = Button(window,width=10,height=4,text="publish",fg="blue",bg="black",command=lambda m="F":client(float(latitud.get("1.0","end-1c")),float(longitud.get("1.0","end-1c")),combo.get()))
         publish.place(x=180,y=350)
         window.mainloop()
     except rospy.ROSInterruptException:
