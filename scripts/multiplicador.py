5dd951f7379fe84f4e1276f421634033c324d9de
QuantumRoboticsURC <quantumrobotics.itesm@gmail.com>
2023-05-05 18:43:19 -0600

    added multiplicador file for mini rover

---
+++ scripts/multiplicador.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+def get_rotation (msg):
+    global vel_z, vel_x
+    vel_x = msg.linear.x 
+    vel_z = msg.angular.z * 1.0
+     
+vel_z = 0.0
+vel_x = 0.0
+rospy.init_node('cmd_vel_multiplier')
+
+sub = rospy.Subscriber ('/cmd_vel', Twist, get_rotation)
+pub = sub = rospy.Publisher ('/mr/cmd_vel', Twist, queue_size=1)
+
+r = rospy.Rate(5)
+cmd_vel_msg = Twist()
+while not rospy.is_shutdown():
+    cmd_vel_msg.angular.z = vel_z
+    cmd_vel_msg.linear.x = vel_x
+    pub.publish(cmd_vel_msg)	
+    r.sleep()
